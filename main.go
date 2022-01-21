93f1fa6a441cc73d5f9023ef524231fd8410ebbc
Iavorskyi Oleksandr <a7038329@gmail.com>
2022-01-21 23:40:56 +0200

    Working version with mux

--- main.go
+++ main.go
@@ -2,59 +2,48 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-
 	"net/http"
-	"os"
-	"strings"
 
 	"github.com/dapr/go-sdk/service/common"
 	daprd "github.com/dapr/go-sdk/service/http"
+	"github.com/gorilla/mux"
 )
 
-var (
-	logger     = log.New(os.Stdout, "", 0)
-	address    = getEnvVar("ADDRESS", ":8080")
-	pubSubName = getEnvVar("PUBSUB_NAME", "pubsub")
-	topicName  = getEnvVar("TOPIC_NAME", "messages")
-)
+//code
+var sub = &common.Subscription{
+	PubsubName: "pubsub",
+	Topic:      "project-lifecycle",
+	Route:      "/checkout",
+}
 
 func main() {
-	// create a Dapr service
-	s := daprd.NewService(address)
+	log.Println("Listening :8000")
+	r := mux.Router(*handlers())
+	s := daprd.NewServiceWithMux(":8000", &r)
 
-	// add some topic subscriptions
-	subscription := &common.Subscription{
-		PubsubName: pubSubName,
-		Topic:      topicName,
-		Route:      fmt.Sprintf("/%s", topicName),
+	err := s.AddTopicEventHandler(sub, eventHandler)
+	if err != nil {
+		log.Fatalf("error adding topic subscription: %v", err)
 	}
 
-	if err := s.AddTopicEventHandler(subscription, eventHandler); err != nil {
-		logger.Fatalf("error adding topic subscription: %v", err)
-	}
-
-	// start the service
 	if err := s.Start(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("error starting service: %v", err)
+		log.Fatalf("error: %v", err)
 	}
 }
 
 func eventHandler(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
-	logger.Printf(
-		"event - PubsubName:%s, Topic:%s, ID:%s, Data: %v",
-		e.PubsubName, e.Topic, e.ID, e.Data,
-	)
-
-	// TODO: do something with the cloud event data
-
+	log.Printf("Subscriber received: %s", e.Data)
 	return false, nil
 }
 
-func getEnvVar(key, fallbackValue string) string {
-	if val, ok := os.LookupEnv(key); ok {
-		return strings.TrimSpace(val)
-	}
-	return fallbackValue
+func myOtherHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("In the other endpoint")
+}
+
+func handlers() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", myOtherHandler).Methods("GET")
+
+	return r
 }
